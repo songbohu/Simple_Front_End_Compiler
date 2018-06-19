ff2805a52d51fb562a9b7edec4db84ca580a014c
Songbo Hu <s1647079@sms.ed.ac.uk>
2018-06-19 17:18:11 +0800

    Support File IO.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 import lexer.Lexer;
 import parser.Parser;
 
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by Mac on 2018/6/16.
@@ -12,6 +12,21 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
+        if(args.length >= 2){
+            try{
+                FileInputStream fis=new FileInputStream(args[0]);
+                System.setIn(fis);}
+            catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
+
+            PrintStream ps=new PrintStream(new FileOutputStream(args[1]));
+            System.setOut(ps);
+
+        }
+
+
         Lexer lex = new Lexer();
         Parser parse = new Parser(lex);
         parse.program();
